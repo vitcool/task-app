1f72be19b54cc356fc30b1840196eb2e14b754cf
vitcool@ukr.net <vitcool@ukr.net>
2020-03-02 15:18:06 +0200

    Tests are improved to use Auth

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest');
 const app = require('../src/app');
+const jsw = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const User = require('../src/models/user');
 
+const userOneId = new mongoose.Types.ObjectId();
+
 const userOne = {
+  _id: userOneId,
   name: 'Teston',
   email: 'vitalikulyk@gmail.com',
   password: 'nkjll12sdasda',
+  tokens: [{
+    token: jsw.sign({ _id: userOneId }, process.env.JWT_SECRET
+  )}]
 };
 
 beforeEach(async () => {
@@ -34,4 +42,34 @@ test('Should non login nonexist user', async () => {
     email: 'hui@ukr.net',
     password: '12323123'
   }).expect(400);
+});
+
+test('Should get user profile', async () => {
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test('Should not get user profile for unauth user', async () => {
+  await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401);
+});
+
+test('Should delete user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test('Should delete user for unauth user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401);
 });
